12360609c996acc0e1a9dcd820ce9be2ad205813
Robert <rsft6000@gmail.com>
2025-01-11 22:52:39 -0600

    feat/cases: eliza starter supabase

--- eliza-starter/src/database/index.ts
+++ eliza-starter/src/database/index.ts
@@ -2,9 +2,16 @@ import { PostgresDatabaseAdapter } from "@elizaos/adapter-postgres";
 import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
 import Database from "better-sqlite3";
 import path from "path";
+// import { SupabaseDatabaseAdapter } from "@elizaos/adapter-supabase";
 
 export function initializeDatabase(dataDir: string) {
   if (process.env.POSTGRES_URL) {
+    // if (process.env.SUPABASE_URL && process.env.SUPABASE_SERVICE_API_KEY) {
+    //   // const db = new SupabaseDatabaseAdapter(
+    //   //   process.env.SUPABASE_URL,
+    //   //   process.env.SUPABASE_SERVICE_API_KEY,
+    //   // );
+    //   // return db;
     const db = new PostgresDatabaseAdapter({
       connectionString: process.env.POSTGRES_URL,
     });
